943792f439ad91eeff106f61347b451dac05f3a8
molspace <nurlybek.amangeldiev@gmail.com>
2024-11-18 17:05:12 +0400

    [feature]: Handling newline chars

--- ascii-art-project-1/main.py
+++ ascii-art-project-1/main.py
@@ -16,23 +16,32 @@ def create_dict(ascii_raw):
         start_index = end_index
     return ascii_dict
 
-def convert_char_to_ascii(input_text: str, ascii_dict: dict):
-    ascii_text_list = (ascii_dict[letter] for letter in input_text)
+def convert_char_to_ascii(word: str, ascii_dict: dict):
+    ascii_text_list = (ascii_dict[letter] for letter in word)
     return ascii_text_list
 
 def join_chars(ascii_text_list):
     ascii_text = '\n'.join([''.join(lines) for lines in zip(*ascii_text_list)])
     return ascii_text
 
-def main(input_text, ascii_raw):    
+def main(input_text, style):
+
+    # hanle newlines
+    input_text = input_text.split('\\n')
+
+    # read style file
+    with open(f'{style}.txt', 'r') as f:
+        ascii_raw = f.readlines()
+
     ascii_dict = create_dict(ascii_raw)
-    ascii_text_list = convert_char_to_ascii(input_text, ascii_dict)
-    ascii_text = join_chars(ascii_text_list)
-    print(ascii_text)
+    for word in input_text:
+        ascii_text_list = convert_char_to_ascii(word, ascii_dict)
+        ascii_text = join_chars(ascii_text_list)
+        print(ascii_text)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description="Process a message with a specified style."
+        description="Process a message with a specified ASCII style."
     )
     parser.add_argument(
         "input_text", 
@@ -47,9 +56,5 @@ if __name__ == '__main__':
         choices=["standard", "shadow", "thinkertoy"],
         help="The style to apply to the input text."
     )
-
-    args = parser.parse_args()
-    with open(f'{args.style}.txt', 'r') as f:
-        ascii_raw = f.readlines()
-    
-    main(args.input_text, ascii_raw)
+    args = parser.parse_args()    
+    main(args.input_text, args.style)
