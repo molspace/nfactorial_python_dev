c936704d90551044dccbde402bf18502cb530bb5
molspace <nurlybek.amangeldiev@gmail.com>
2024-11-18 16:22:59 +0400

    [feature]: ASCII added bonus styles

--- ascii-art-project-1/main.py
+++ ascii-art-project-1/main.py
@@ -2,6 +2,9 @@
 # It parses ASCII styled chars from txt files and 
 # creates a char dict to convert and print the inputted text.
 
+import argparse
+
+
 # create a dict of char: ascii
 def create_dict(ascii_raw):
     ascii_dict = {}
@@ -21,15 +24,32 @@ def join_chars(ascii_text_list):
     ascii_text = '\n'.join([''.join(lines) for lines in zip(*ascii_text_list)])
     return ascii_text
 
-def main():
-    # read ascii file
-    with open('standard.txt', 'r') as f:
-        ascii_raw = f.readlines()
-    
+def main(input_text, ascii_raw):    
     ascii_dict = create_dict(ascii_raw)
-    ascii_text_list = convert_char_to_ascii('abc def xyz !~^', ascii_dict)
+    ascii_text_list = convert_char_to_ascii(input_text, ascii_dict)
     ascii_text = join_chars(ascii_text_list)
     print(ascii_text)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description="Process a message with a specified style."
+    )
+    parser.add_argument(
+        "input_text", 
+        type=str, 
+        help="The input text to be processed."
+    )
+    parser.add_argument(
+        "style", 
+        type=str, 
+        nargs="?",
+        default='standard',
+        choices=["standard", "shadow", "thinkertoy"],
+        help="The style to apply to the input text."
+    )
+
+    args = parser.parse_args()
+    with open(f'{args.style}.txt', 'r') as f:
+        ascii_raw = f.readlines()
+    
+    main(args.input_text, ascii_raw)
