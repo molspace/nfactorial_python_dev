0daf54ae370164f581dc1203986187eb55c873d7
Nurlybek Temirbay <33928481+molspace@users.noreply.github.com>
2025-04-11 11:09:27 +0400

    Delete backend_final_project/streaker directory

--- backend_final_project/streaker/app/db/base.py
+++
@@ -1,3 +0,0 @@
-from app.models.user import User  # import all models here
-
-from app.db.session import Base
--- backend_final_project/streaker/app/schemas/friendship.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class FriendAdd(BaseModel):
-    friend_id: str  # or friend_email
-
-class FriendRead(BaseModel):
-    id: str
-    user_id: str
-    friend_id: str
